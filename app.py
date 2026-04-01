ec17e751d854be9dcb7173446827a0dcba4ac571
raghava3333 <munagapatiragahavsai@gmail.com>
2026-04-01 16:49:06 +0530

    clean backend without torch

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 from pillow_heif import register_heif_opener
 register_heif_opener()
+
 from flask import Flask, request, jsonify
 from PIL import Image
 import os
 import io
-import torchvision.transforms as transforms
 
 app = Flask(__name__)
-
-# Load model
-model = torch.load("full_model_eff.pth", map_location='cpu')
-model.eval()
-
-# Class names (VERY IMPORTANT)
-class_names = [
-    'Anthracnose fruit',
-    'Anthracnose leaf',
-    'Bacterial Canker fruit',
-    'Gall_Mid leaf',
-    'Healthy fruit',
-    'Healthy leaf'
-]
-
-# Transform
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
 
 # ✅ MAIN ROUTE
 @app.route('/predict', methods=['POST'])
@@ -36,9 +16,6 @@ def predict():
     image = Image.open(io.BytesIO(file_bytes)).convert('RGB')
     
     return{"message":"API working"}
-    image = transform(image).unsqueeze(0)
-
-    result = class_names[predicted.item()]
 
     print("prediction", result)
 
